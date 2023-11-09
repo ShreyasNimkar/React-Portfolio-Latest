b15102330477818ac4ed7d0a2be60aba73105e90
ShreyasNimkar <nimkarshreyas22@gmail.com>
2023-11-09 23:20:48 +0530

    feat:initial commit

---
+++ src/projectData.js
@@ -0,0 +1,21 @@
+const projects = [
+  {
+    title: "MERN Support Ticket App",
+    image: "./images/projects1.jpeg",
+    description: "Website to post your grievences for technical support.",
+    tools: ["React", "Redux", "MongoDB", "JWT", "Express"],
+    github: "https://github.com/csivitu/yantra",
+    link: "https://github.com/csivitu/yantra",
+  },
+  {
+    title: "Yantra Website",
+    image: "./images/projects2.jpeg",
+    description:
+      "Hackathon Management Portal for VIT's official largest hackathon",
+    tools: ["NEXT", "API", "Tailwind"],
+    github: "https://github.com/ShreyasNimkar/React-Support-Ticket-System",
+    link: "https://github.com/ShreyasNimkar/React-Support-Ticket-System",
+  },
+];
+
+export default projects;
